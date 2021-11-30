419c35dc7bc5b4041e1ee245b31621b51a484edd
Attaphong Rattanaveerachanon <ohm.attaphong@gmail.com>
2021-11-30 23:38:03 +0700

    Add MVI, RLC, DAD, LDAX, STAX instructions

--- processor.go
+++ processor.go
@@ -56,13 +56,25 @@ func (p *Processor) Run() {
 	case 0x01:
 		p.lxi(&p.B, &p.C)
 	case 0x02:
-		p.unimplemented()
+		p.stax(&p.B, &p.C)
 	case 0x03:
 		p.inx(&p.B, &p.C)
 	case 0x04:
 		p.inr(&p.B)
 	case 0x05:
 		p.dcr(&p.B)
+	case 0x06:
+		p.mvi(&p.B)
+	case 0x07:
+		p.rlc()
+	case 0x08:
+		p.nop()
+	case 0x09:
+		p.dad(&p.B, &p.C)
+	case 0x10:
+		p.ldax(&p.B, &p.C)
+	default:
+		p.unimplemented()
 	}
 }
 
@@ -90,25 +102,69 @@ func (p *Processor) inx(msb *byte, lsb *byte) {
 // Increase value of 8-bit register by 1
 func (p *Processor) inr(reg *byte) {
 	p.dasm("INR")
-	result := uint16(*reg) + 1
-	*reg = byte(result & 0x00ff)
+	result16 := uint16(*reg) + 1
+	*reg = byte(result16 & 0x00ff)
 
-	// TODO - calculate affected flags: z, s, p, aux
 	p.SetZero(*reg)
 	p.SetSign(*reg)
 	p.SetParity(*reg)
+	p.SetAuxiliaryCarry(result16)
 }
 
 // Decrease value of 8-bit register by 1
 func (p *Processor) dcr(reg *byte) {
 	p.dasm("DCR")
-	result := uint16(*reg) - 1
-	*reg = byte(result & 0xff)
+	result16 := uint16(*reg) - 1
+	*reg = byte(result16 & 0xff)
 
-	// TODO - calculate affected flags: z, s, p, aux
+	p.SetZero(*reg)
+	p.SetSign(*reg)
+	p.SetParity(*reg)
+	p.SetAuxiliaryCarry(result16)
+}
+
+// Move immediate data
+func (p *Processor) mvi(reg *byte) {
+	p.dasm("MVI")
+	*reg = p.mmu.Memory[p.PC]
+	p.PC += 1
+}
+
+// Rotate accumulator left
+func (p *Processor) rlc() {
+	p.dasm("RLC")
+	aux := p.A
+	p.A = aux<<1 | aux>>7
+	p.Carry = (aux >> 7) > 0
+}
+
+// Double Add - add specified register pair to HL
+func (p *Processor) dad(msb *byte, lsb *byte) {
+	p.dasm("DAD")
+	adder := uint32(*msb)<<8 | uint32(*lsb)
+	result := uint32(p.H)<<8 | uint32(p.L)
+	result += adder
+	p.H = byte(result >> 8)
+	p.L = byte(result & 0xFF)
+	p.Carry = result > 0xFFFF // greater than 2-byte
 
 }
 
+// Load Accumulator - load data from the provided address
+func (p *Processor) ldax(msb *byte, lsb *byte) {
+	p.dasm("LDAX")
+	address := uint16(*msb)<<8 | uint16(*lsb)
+	p.A = p.mmu.Memory[address]
+}
+
+// Store Accumulator - store data from A to the provided address
+func (p *Processor) stax(msb *byte, lsb *byte) {
+	p.dasm("STAX")
+	address := uint16(*msb)<<8 | uint16(*lsb)
+	p.mmu.Memory[address] = p.A
+}
+
+// Unimplemented
 func (p *Processor) unimplemented() {
 	p.dasm(fmt.Sprintf("%02x:UNIMPLEMENTED", p.mmu.Memory[p.PC]))
 }
@@ -146,5 +202,6 @@ func (p *Processor) dasm(opcode string) {
 	if !p.DebugMode {
 		return
 	}
+
 	fmt.Printf("%s\n", opcode)
 }
